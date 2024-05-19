5405f99470990bd32970269ab7d6fccb1746cac4
vinaybv <vinay.bv@accionlabs.com>
2024-05-19 10:50:32 +0530

    d

--- src/common/axios.js
+++ src/common/axios.js
@@ -50,22 +50,9 @@ AxiosMain.interceptors.response.use(
   },
 );
 
-AxiosCommon.interceptors.response.use(
-  function (response) {
-    return response?.data;
-  },
-  function (error) {
-    const { response } = error;
-    if (response.status === 401) {
-      localStorage.removeItem('filtered_symbols');
-      localStorage.removeItem('user');
-      localStorage.removeItem('symbols');
-      localStorage.removeItem('token');
-      window.location.href = window.location.origin;
-    }
-    return Promise.reject(error);
-  },
-);
+AxiosCommon.interceptors.response.use(function (response) {
+  return response?.data;
+});
 
 export const axios = AxiosMain;
 export const axiosCommon = AxiosCommon;
